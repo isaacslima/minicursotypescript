7f6522644ecba1f11d18205f960edcd6b29dde13
Isaac de Sousa <isaacslima@yahoo.com.br>
2020-07-05 17:31:05 -0300

    type alias

---
+++ logDetails.ts
@@ -0,0 +1,25 @@
+// type alias
+type Uid = number | string | undefined
+
+function logDetails(uid: Uid, item: string){
+    console.log(`A product with ${uid} has a title as ${item}.`)
+}
+
+function logInfo(uid: Uid, user: string){
+    console.log(`A user with ${uid} has a title as ${user}.`)
+}
+
+type Platform = 'Windows' | 'Linux' | 'Mac OS'
+
+function renderPlatform(platform: Platform){
+    return platform
+}
+
+renderPlatform('Windows');
+
+logDetails(123, "sapato")
+logDetails('123', "sapato")
+
+
+logInfo(123, "Isaac")
+logInfo('123', "Isaac")
